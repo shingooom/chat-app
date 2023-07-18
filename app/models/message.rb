0d0975af675a688d58aed86f045b7980ae924b11
shingooom <sng.med@gmail.com>
2023-07-18 16:19:42 +0900

    messagesテーブルの作成

---
+++ app/models/message.rb
@@ -0,0 +1,13 @@
+class Message < ApplicationRecord
+  belongs_to :room
+  belongs_to :user
+  
+  def change
+    create_table :messages do |t|
+      t.string  :content
+      t.references :room, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_034252) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_071245) do
+  create_table "messages", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "room_users", charset: "utf8", force: :cascade do |t|
     t.bigint "room_id", null: false
     t.bigint "user_id", null: false
