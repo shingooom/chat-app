9e430b96bd4be5b5925a857d504555a4961eb724
shingooom <sng.med@gmail.com>
2023-07-18 19:24:34 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,14 +3,9 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
 
-  def change
-    create_table :messages do |t|
-      t.string  :content
-      t.references :room, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
-      t.timestamps
-    end
+  def was_attached?
+    self.image.attached?
   end
 end
